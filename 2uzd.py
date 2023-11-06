42e431f292119a0e0d9f0f3bf534d32645df752e
Sanija Sokirka <sanijasokirka@gmail.com>
2023-11-06 09:20:07 +0200

    2uzd

--- 2uzd.py
+++ 2uzd.py
@@ -1,3 +1,12 @@
 """
 Aprēķināt noteiktas skaitļu summu no 1 līdz n, kur n ir lietotāja ievadīts skaitlis.
 """
+
+n=int(input("Ievadi skaitli: "))
+summa=0
+# 1----5---1+2+3+4
+
+for skaitlis in range (1,n+1):
+    summa+=skaitlis
+
+print(f"Skaitļu no 1 līdz {n} summa ir {summa}.")
