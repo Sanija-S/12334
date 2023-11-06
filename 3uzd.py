192e26235cc459ff12ef985c632af6608ad5b46b
Sanija Sokirka <sanijasokirka@gmail.com>
2023-11-06 09:30:50 +0200

    13123

--- 3uzd.py
+++ 3uzd.py
@@ -1,3 +1,20 @@
 """
 Izvadīt visus pirmsskaitļus no 1 līdz 100, izmantojot for un if.
+
+1) kas ir pirmskaitlis- skaitlis lielaks par 1 kas dalas ar 2 skaitliem
+20 apgabals....1 lidz 100
+3.0 priekšraksti for un if
+
+# nosaku gala punktu, **- kvadrat sakne 
 """
+
+def ir_pirmskaitlis(skaitlis):
+    if skaitlis<2:
+        return False
+    for dalītajs in range(2, int(skaitlis**0.5)+1) 
+     if skaitlis% dalītajs==0:
+        return False
+     return True
+    for skaitlis in range(1,101):
+       if ir_pirmskaitlis(skaitlis):
+          print(skaitlis, end= " ")
