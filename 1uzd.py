9d1c681187e17b4cb442d7db39d4e8f3ff42a04e
Sanija Sokirka <sanijasokirka@gmail.com>
2023-11-06 09:12:18 +0200

    uzd 1 izmainiits

--- 1uzd.py
+++ 1uzd.py
@@ -2,3 +2,10 @@
 Ja vēlaties iegūt nejaušu skaitli no 1 līdz 10.
 
 """
+"""jābūt iespējai izdrukāt satora izvēlētu skaitli, robezās no 1 lidz 10."""
+
+
+import random
+
+nejauss_skaitlis=random.randint(1,10)
+print(nejauss_skaitlis)
